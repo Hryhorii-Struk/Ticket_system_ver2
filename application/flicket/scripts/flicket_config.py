7c2b602ac5162dad63806473a9cc08594e813b6b
Hryhorii-Struk <dracondhbsh3@gmail.com>
2024-05-27 10:34:32 +0300

    Update flicket_config.py

--- application/flicket/scripts/flicket_config.py
+++ application/flicket/scripts/flicket_config.py
@@ -1,7 +1,4 @@
-#! usr/bin/python3
-# -*- coding: utf-8 -*-
-#
-# Flicket - copyright Paul Bourne: evereux@gmail.com
+
 
 from application import app
 from application.flicket_admin.models.flicket_config import FlicketConfig
